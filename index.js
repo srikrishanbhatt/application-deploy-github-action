1cffa46fd75580be87b78f4a951f03ba434a8026
Srikrishan Bhatt <srikrishnabhatt@gmail.com>
2026-03-29 18:21:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    return res.json({msg: 'Hello from the server'});
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is up and running on port ${PORT}`)
+})
