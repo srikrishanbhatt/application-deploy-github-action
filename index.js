db339402a40890319b776f28832112d044d4ee3f
Srikrishan Bhatt <srikrishnabhatt@gmail.com>
2026-06-28 13:24:37 +0530

    Configure EC2 deployment workflow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    return res.json({msg: 'Hello from the server'});
+    return res.json({msg: 'Hello from the server check CI/CD working...'});
 })
 
 app.listen(PORT, () =>{
