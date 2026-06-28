5ec74aab2a216d206cc02c7a386285ef8d9c12e1
Srikrishan Bhatt <srikrishnabhatt@gmail.com>
2026-06-28 13:58:12 +0530

    Response message updates

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  return res.json({ msg: 'Hello from the server check CI/CD working...' });
+  return res.json({ msg: 'Hello from the server check CI and CD working...' });
 });
 
 export default app;
